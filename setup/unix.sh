b618d5971b05ccc9193dddd7292e51ae65ba4062
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-11-29 10:59:23 -0500

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -14,9 +14,9 @@ haxelib install flixel-addons 2.11.0
 haxelib install flixel-tools
 haxelib install flixel-ui 2.6.1
 haxelib install actuate 1.9.0 
-haxelib install hxCodec 2.5.1          
-haxelib install linc_luajit
+haxelib install hxCodec 2.5.1
 haxelib install hscript
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
 haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
